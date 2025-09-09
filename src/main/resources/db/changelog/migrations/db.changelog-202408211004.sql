063636bea205784a1d322d04a05a91fedad2ad25
Vinicius Alvarez <110983068+VinizAA@users.noreply.github.com>
2025-09-09 17:05:52 -0300

    Update db.changelog-202408211004.sql

--- src/main/resources/db/changelog/migrations/db.changelog-202408211004.sql
+++ src/main/resources/db/changelog/migrations/db.changelog-202408211004.sql
@@ -5,9 +5,16 @@
 CREATE TABLE CARDS(
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
+    description TEXT,
     board_column_id BIGINT NOT NULL,
-    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE
+    priority VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    assigned_to BIGINT,
+    CONSTRAINT boards_columns__cards_fk FOREIGN KEY (board_column_id) REFERENCES BOARDS_COLUMNS(id) ON DELETE CASCADE,
+    CONSTRAINT users__cards_fk FOREIGN KEY (assigned_to) REFERENCES USERS(id),
+    CONSTRAINT board_column_position_uk UNIQUE KEY (board_column_id, position)
 ) ENGINE=InnoDB;
+
 
 --rollback DROP TABLE CARDS
